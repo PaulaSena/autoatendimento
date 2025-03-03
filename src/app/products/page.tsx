e7fec7efe766ad341be134575b642c9dea7622b2
PaulaSena <senasilvaanapaula@gmail.com>
2025-03-03 18:39:49 -0300

    feat:add shadcn initial setup and components

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 const ProductPage = () => {
     return(
     <div className="p-5">
-        <h1>Product Page</h1>
+        <h1> Pagina de Produtos</h1>
         <Button className="m-2">Buy Now</Button>
         <Input className="m-2" placeholder="Bora nessa"/>
     </div> );    
